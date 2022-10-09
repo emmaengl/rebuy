bd5078c3f30cb3d4a6421f1a877067964ffff55f
emmaengl <emma@englishby.co>
2022-10-09 15:05:08 +0200

    Adds product service

---
+++ src/app/products/products.service.ts
@@ -0,0 +1,29 @@
+import { HttpClient, HttpErrorResponse } from '@angular/common/http';
+
+import { throwError } from 'rxjs';
+import { catchError, retry } from 'rxjs/operators';
+
+import { Product } from "./product";
+
+export class ProductsService {
+  productsUrl = 'https://fakestoreapi.com/products';
+
+  constructor(private http: HttpClient) { }
+
+  getProducts() {
+    return this.http.get<Product[]>(this.productsUrl)
+      .pipe(
+        retry(3), // retry a failed request up to 3 times
+        catchError(this.handleError) // then handle the error
+      );
+  }
+
+  private handleError(error: HttpErrorResponse) {
+    if (error.status === 0) {
+      console.error('An error occurred:', error.error);
+    } 
+    
+    // Return an observable with a user-facing error message.
+    return throwError(() => new Error('Something bad happened; please try again later.'));
+  }
+}
