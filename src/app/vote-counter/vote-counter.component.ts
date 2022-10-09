acfd87c183b61e426f23911c2d18b91d7bcff741
emmaengl <emma@englishby.co>
2022-10-09 15:57:44 +0200

    Add data input and counter functions

--- src/app/vote-counter/vote-counter.component.ts
+++ src/app/vote-counter/vote-counter.component.ts
@@ -1,15 +1,25 @@
-import { Component, OnInit } from '@angular/core';
+import { Component, Input } from '@angular/core';
 
 @Component({
   selector: 'app-vote-counter',
   templateUrl: './vote-counter.component.html',
   styleUrls: ['./vote-counter.component.css']
 })
-export class VoteCounterComponent implements OnInit {
 
+export class VoteCounterComponent {
   constructor() { }
 
-  ngOnInit(): void {
+  @Input() count = 0;
+
+  incrementVotes() {
+    this.count++
+
+    // will need to push new value to db
   }
 
+  decrementVotes() {
+    this.count--
+
+    // will need to push new value to db
+  }
 }
