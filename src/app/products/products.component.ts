b34d4d9cc607549dc0ce1b701c05ed651f97d241
emmaengl <emma@englishby.co>
2022-10-09 15:18:26 +0200

    Injects product data into component

--- src/app/products/products.component.ts
+++ src/app/products/products.component.ts
@@ -1,15 +1,29 @@
 import { Component, OnInit } from '@angular/core';
 
+import { Product } from './product';
+import { ProductsService } from './products.service';
+
 @Component({
   selector: 'app-products',
   templateUrl: './products.component.html',
+  providers: [ProductsService],
   styleUrls: ['./products.component.css']
 })
+
 export class ProductsComponent implements OnInit {
+  products: Product[] = [];
+  
+  constructor(private productsService: ProductsService) {}
 
-  constructor() { }
-
-  ngOnInit(): void {
+  ngOnInit() {
+    this.getProducts();
   }
 
+  getProducts(): void {
+    this.productsService.getProducts()
+      .subscribe(products => {
+        console.log(products)
+        return this.products = products
+      });
+  }
 }
